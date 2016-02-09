6467706173e22a1fee900aeb91c8acc62dcb0505
orlandoc01 <orlandoc@sas.upenn.edu>
2016-02-08 19:52:40 -0800

    Routes logout button to /signout

--- shortly.js
+++ shortly.js
@@ -144,6 +144,14 @@ app.post('/signup', function(req, res) {
     });
 });
 
+
+app.get('/signout', 
+function(req, res) {
+  req.session.destroy();
+  res.redirect('/login');
+});
+
+
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
 // assume the route is a short code and try and handle it here.
